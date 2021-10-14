ebf74c782ae0805a0b89a61a7cbad2dd4266b074
shiv pratap <shiv0199.cse19@chitkara.edu.in>
2021-10-14 11:47:03 +0530

    r

--- src/components/ContactCard.js
+++ src/components/ContactCard.js
@@ -1,20 +1,20 @@
 import React from 'react'
 import abc from '../images/abc.jpg'
-const CardContact = (props) =>{
-    const {id,name,email}=props.contact;
+const CardContact = (props) => {
+    const { id, name, email } = props.contact;
     return (
         <div className="item">
             <img className="ui avatar image" src={abc} alt="user" />
-                <div className="content">
-                    <div className="header">{name}</div>
-                    <div>{email}</div>
-                </div>
-                <i 
-                className="trash alternate outline icon" 
-                style = {{color:"red", marginTop:"7px"}}
-                onclick={()=>props.clickHandler(id)}
-                ></i>
+            <div className="content">
+                <div className="header">{name}</div>
+                <div>{email}</div>
             </div>
+            <i
+                className="trash alternate outline icon"
+                style={{ color: "red", marginTop: "7px" }}
+                onclick={() => props.clickHandler(id)}
+            ></i>
+        </div>
     );
 };
 
