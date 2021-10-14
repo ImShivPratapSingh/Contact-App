5a297afa2569aa0d11cbe98a114eb7f352687e38
shiv pratap <shiv0199.cse19@chitkara.edu.in>
2021-10-14 10:49:20 +0530

    contact

---
+++ src/components/ContactCard.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import abc from '../images/abc.jpeg'
+const CardContact = (props) =>{
+    const {id,name,email}=props.contact;
+    return (
+        <div className="item">
+            <img className="ui avatar image" src={abc} alt="user" />
+                <div className="content">
+                    <div className="header">{name}</div>
+                    <div>{email}</div>
+                </div>
+                <i 
+                className="trash alternate outline icon" 
+                style = {{color:"red", marginTop:"7px"}}
+                onclick={()=>props.clickHandler(id)}
+                ></i>
+            </div>
+    );
+};
+
+export default CardContact;
